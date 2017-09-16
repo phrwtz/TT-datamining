cef20ff9ed38952c615fb79def51d71b86c6e60e
phorwitz <phorwitz@concord.org>
2017-09-16 10:48:53 -0400

    Fixed bug that was causing some varRefs not to be reported.

--- report-tools.js
+++ report-tools.js
@@ -372,7 +372,7 @@ function reportVarRefs(teams) {
 
 function variableInVarRef(vrStr, vrArray) { //Looks for the vrStr in vrArray. Returns true if found.
     for (var i = 0; i < vrArray.length; i++) {
-        if (vrArray[i][1][1] == vrStr) {
+        if (vrArray[i][0][1] == vrStr) {
             return true;
         }
     }
